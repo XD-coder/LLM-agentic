6159d62d2bc851e79616c216567c9489985efed4
XD-coder <78638941+XD-coder@users.noreply.github.com>
2024-06-19 15:10:39 +0530

    Update agents.py

--- agents.py
+++ agents.py
@@ -1,12 +1,21 @@
+import LLM_connect
+from groq import AsyncGroq
+
+client = AsyncGroq(api_key=)
+
 class agents :
     """
     """
-    def __init__(self,description, system_prompt, prompt_templates,max_iteration = 10 , stop_patterns = None ) -> None:
+    def __init__(self,description, system_prompt, prompt_templates,api_to_use = "" , model_to_use = "" ,max_iteration = 10 , stop_patterns = None ) -> None:
         self.desc = description
         self.sys_prompt = system_prompt
         self.template = prompt_templates
         self.max_iter = max_iteration
         self.stop_sequence = stop_patterns
         
-    def agent_start(self):
-        pass
+    async def agent_start(self):
+        await call_LLM()
+    
+async def call_LLM(chosen_api="", chosen_model=""):
+    if chosen_api == "":
+        
