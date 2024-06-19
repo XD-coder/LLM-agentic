b0cc0f2c5761983f7ef8abc64e90fb92dab230d0
Kartikey Mishra <78638941+XD-coder@users.noreply.github.com>
2024-06-19 15:38:16 +0530

    Update LLM_connect.py

--- LLM_connect.py
+++ LLM_connect.py
@@ -4,7 +4,7 @@
 
 from groq import AsyncGroq
 
-client = AsyncGroq(api_key="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")
+client = AsyncGroq(api_key="")
 
 async def call_groq(prompt, modl, max_tokens    ):
     chat_completion = await client.chat.completions.create(messages=prompt, model="mixtral-8x7b-32768", max_tokens=200, stop=[r"/s"])
